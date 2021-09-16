0052054f08f14575945f4215466d254cac791ecb
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-09-16 10:49:52 +0000

    Fixed isCompleted bug.

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const PORT = 4000;
 
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 ['todo', 'task', 'user'].forEach((type) => app.use(`/${type}`, genRouter(type)));
 
 app.listen(PORT, () => console.log(`server listening to ${PORT}`));
